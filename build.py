7aaae2183db60a6852bce0c2ade3e57aa6003c6b
lovezsh <shuxhan@163.com>
2023-03-09 11:23:19 +0800

    Update build.py

--- build.py
+++ build.py
@@ -28,9 +28,6 @@ if __name__ == "__main__":
         dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
         num = int(oldTitle.split('-')[0])
         if index < 5 :
-          if num < 100 :
-            modified = dateList[99-num]
-          else :
             modified = fetch_ci_time('/src/pages/posts/' + filepath)
 
           recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
