2af52d3d45afee241f5e6f5e0541a2a2958b2dd7
lovezsh <shuxhan@163.com>
2023-03-09 12:34:32 +0800

    Update build.py

--- build.py
+++ build.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
       if name.endswith('.md'):
         filepath = urllib.parse.quote(name)
         oldTitle = name.split('.md')[0]
-        url   = 'https://zweekly.netlify.app/posts/' + oldTitle
+        url   = 'https://zweekly.vercel.app/posts/' + oldTitle
         title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
         readmeMd= '* [{}]({})\n'.format(title, url)
         dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
